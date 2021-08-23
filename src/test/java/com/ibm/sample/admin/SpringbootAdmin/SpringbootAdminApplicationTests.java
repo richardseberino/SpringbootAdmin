88fffdb1e38c7f91eb4db8c5bb1bc9dc3c98c354
Richard Marques <richard@seberino.com.br>
2021-08-23 14:02:30 -0300

    First Commit

---
+++ src/test/java/com/ibm/sample/admin/SpringbootAdmin/SpringbootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.sample.admin.SpringbootAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
